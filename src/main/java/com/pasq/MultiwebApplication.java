f514331d40358ec1847042de20be8aac144a38a6
wwx <w17731138318@qq.com>
2018-01-15 11:40:52 +0800

    Initial commit

---
+++ src/main/java/com/pasq/MultiwebApplication.java
@@ -0,0 +1,12 @@
+package com.pasq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiwebApplication.class, args);
+	}
+}
