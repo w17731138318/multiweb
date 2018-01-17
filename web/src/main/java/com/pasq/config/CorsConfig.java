1396921df9b3e9a235e5924e5dc3ee19a612e6b7
wwx <w17731138318@qq.com>
2018-01-17 14:45:38 +0800

    spring boot 设置本质就是一个拦截器

--- web/src/main/java/com/pasq/config/CorsConfig.java
+++ web/src/main/java/com/pasq/config/CorsConfig.java
@@ -6,13 +6,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 /**
  * 允许跨域
- *
+ * 此拦截器可以移至 WebMvcConfig 配置类中
+ * @see WebMvcConfig
  * @author 王伟鑫
  * @email wangweixin@pasq.com
  * @date 2017年11月29日
  */
 @Configuration
 public class CorsConfig extends WebMvcConfigurerAdapter {
+    /**
+     * {@link CorsRegistry}
+     * @param registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
